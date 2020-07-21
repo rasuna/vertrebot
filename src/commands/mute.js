6fff9a76881679af18530de98816f99b718a8792
Rasuna Khatami <rasuna.khatami@wearekettle.com>
2020-07-21 14:28:27 -0400

    Add member permission check

--- src/commands/mute.js
+++ src/commands/mute.js
@@ -2,7 +2,15 @@ module.exports = {
   name: 'mute',
   description: 'Mute everyone in the voice channel',
   execute(message) {
-    const channel = message.member.voice.channel;
+    const member = message.member;
+
+    // Check member permission
+    if (!member.hasPermission('MUTE_MEMBERS')) {
+      return message.reply('You do not have permission to perform that command!');
+    }
+
+    // Mute members
+    const channel = member.voice.channel;
     channel.members.forEach((member) => {
       member.voice.setMute(true);
     });
